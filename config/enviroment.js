d84e0981b786843774af09fdcde9e8a951ad903c
Saurabh Srivastava <saurabhsri98@gmail.com>
2023-05-21 18:27:40 +0530

    Update enviroment.js

--- config/enviroment.js
+++ config/enviroment.js
@@ -14,7 +14,7 @@ const devlopment={
 
     google_client_id: "297293548800-9531e9p5lt38mc702c5mkb0o6a6g0i71.apps.googleusercontent.com",
     google_client_secret: "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",
-    google_call_back_url: "http://localhost:1700/users/auth/google/callback"
+    google_call_back_url: "http://localhost:8003/users/auth/google/callback"
         
     
 }
